c5039b2dde60b0f8eb92acc47245a4b56a135af5
Mini <minnnidev@gmail.com>
2024-05-07 23:47:29 +0900

    create project

---
+++ RxMemo/RxMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxMemo
+//
+//  Created by 김민 on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
